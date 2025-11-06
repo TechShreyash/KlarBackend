cf067c6b800ac1cd47ab6d4b8aa7135345551219
Shreyash Kumar Dheemar <techshreyash123@gmail.com>
2025-11-06 15:29:25 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-GOOGLE_API_KEYS = [
-    "AIzaSyAJp2lvHpAxWip3Kx8HcgHuXMa0VEkKH6c",
-    "AIzaSyCfB-ynO2k7Ftqmw5qyiWLcyPmOguBM3gc",
-    "AIzaSyAz-l4BJw7gwl54NYhS-WUSj3WA2qTT2xo",
-    "AIzaSyB6V_eBNXatnTlSPW3EwaWxSFgVTVOD_QM",
-    "AIzaSyBR_pXI43j5MRYAITr95WMBlji2TsnbT8Q",
-    "AIzaSyDZAwvwEF1oPDImJSeRmanR4dhrbs2qxJY",
-]
